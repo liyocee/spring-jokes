22874102bc2cc8afba3a5a9065f42e391a905956
Collins Liyosi <collinskivale@gmail.com>
2018-08-19 17:15:49 +0300

    Initialize project

---
+++ src/main/java/com/liyosi/springjokes/SpringJokesApplication.java
@@ -0,0 +1,12 @@
+package com.liyosi.springjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJokesApplication.class, args);
+	}
+}
