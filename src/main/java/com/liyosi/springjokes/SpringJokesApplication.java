71ae733c88d400f4b82b4ea12bd5bd01727f25f2
Collins Liyosi <collinskivale@gmail.com>
2018-08-20 15:55:52 +0300

    Add spring xml based configuration

--- src/main/java/com/liyosi/springjokes/SpringJokesApplication.java
+++ src/main/java/com/liyosi/springjokes/SpringJokesApplication.java
@@ -2,8 +2,10 @@ package com.liyosi.springjokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringJokesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/liyosi/springjokes/config/ChuckConfiguration.java
+++ src/main/java/com/liyosi/springjokes/config/ChuckConfiguration.java
@@ -1,17 +1,15 @@
 package com.liyosi.springjokes.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by liyosi on Aug, 2018
  */
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-  @Bean
+//  @Bean
   public ChuckNorrisQuotes chuckNorrisQuotes() {
     return new ChuckNorrisQuotes();
   }
